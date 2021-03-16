a1281939cb7d6f99bde98fe8228c996c6e717404
Md. Mostak Shahid <mostak.shahid@gmail.com>
2021-03-16 16:38:21 +0600

    1.0.3

---
+++ script.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function($){
+    /*$('.product-carousel-container').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: $(this).siblings().find('.prev'),
+        nextArrow: $(this).siblings().find('.next'),
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ],
+    });*/
+});
