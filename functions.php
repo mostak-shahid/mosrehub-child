fa2b237ec90302241a315abb8b6ea836b54fa00d
Md. Mostak Shahid <mostak.shahid@gmail.com>
2021-03-16 16:44:07 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ function enqueue_parent_theme_style() {
     
     wp_enqueue_style( 'slick', get_stylesheet_directory_uri() . '/plugins/slick/slick.css' );
     wp_enqueue_style( 'slick-theme', get_stylesheet_directory_uri() . '/plugins/slick/slick-theme.css' );
-    wp_enqueue_script('slick', get_stylesheet_directory_uri() . '/plugins/slick/slick.min.js', 'jquery');
+    wp_enqueue_script('slick', get_stylesheet_directory_uri() . '/plugins/slick/slick.min.js', 'jquery-core');
     
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
     
