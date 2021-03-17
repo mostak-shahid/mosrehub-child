e72f3aebdce1ab1ee1c569c302b7bee1a0594c98
Md. Mostak Shahid <mostak.shahid@gmail.com>
2021-03-17 16:29:41 +0600

    Update carbon-fields.php

--- carbon-fields.php
+++ carbon-fields.php
@@ -5,6 +5,18 @@ use Carbon_Fields\Field;
 
 add_action( 'carbon_fields_register_fields', 'crb_attach_theme_options' );
 function crb_attach_theme_options() {
+    $products = [];
+    $args = array( 
+		'post_type' 		=> 'product',
+		'posts_per_page' => -1,
+	);
+    $query = new WP_Query( $args );
+    if ( $query->have_posts() ) :
+        while ( $query->have_posts() ) : $query->the_post();
+            $products[get_the_id()] = get_the_title();
+        endwhile;
+        wp_reset_postdata();
+    endif;
     /*Container::make( 'theme_options', __( 'Theme Options', 'crb' ) )
         ->add_fields( array(
             Field::make( 'text', 'crb_text', 'Text Field' ),
@@ -16,27 +28,19 @@ function crb_attach_theme_options() {
                 ->set_position( 37.423156, -122.084917, 14 ),
             Field::make( 'sidebar', 'crb_custom_sidebar' ),
             Field::make( 'image', 'crb_photo' ),
-        ));
-    Block::make( __( 'Mos Image Block' ) )
+        ));*/
+    Block::make( __( 'Mos Deal of the Day' ) )
     ->add_fields( array(
-        Field::make( 'text', 'mos-image-heading', __( 'Heading' ) ),
-        Field::make( 'image', 'mos-image-media', __( 'Image' ) ),
-        Field::make( 'rich_text', 'mos-image-content', __( 'Content' ) ),
-        //Field::make( 'color', 'mos-image-hr', __( 'Border Color' ) ),
-        Field::make( 'text', 'mos-image-btn-title', __( 'Button' ) ),
-        Field::make( 'text', 'mos-image-btn-url', __( 'URL' ) ),
-        Field::make( 'select', 'mos-image-alignment', __( 'Content Alignment' ) )
-            ->set_options( array(
-                'left' => 'Left',
-                'right' => 'Right',
-                'center' => 'Center',
-            ))
+        Field::make( 'text', 'mos-dtd-heading', __( 'Heading' ) ),
+        Field::make( 'date_time', 'mos-dtd-heading', __( 'Estimated time of ending' ) ),
+        Field::make( 'select', 'mos-dtd-product', __( 'Select a product' ) )
+            ->set_options( $products )
     ))
-    ->set_icon( 'id-alt' )
+    ->set_icon( 'cart' )
     ->set_render_callback( function ( $fields, $attributes, $inner_blocks ) {
         ?>
-        <div class="mos-image-block-wrapper <?php echo $attributes['className'] ?>">
-            <div class="mos-image-block text-<?php echo esc_html( $fields['mos-image-alignment'] ) ?>">
+        <div class="mos-image-dtd-wrapper <?php echo $attributes['className'] ?>">
+            <div class="mos-image-block">
                 <div class="img-part"><?php echo wp_get_attachment_image( $fields['mos-image-media'], 'full' ); ?></div>
                 <div class="text-part">
                     <h4><?php echo esc_html( $fields['mos-image-heading'] ); ?></h4>
@@ -51,7 +55,7 @@ function crb_attach_theme_options() {
             </div>
         </div>
         <?php
-    });*/
+    });
     
 }
 add_action( 'after_setup_theme', 'crb_load' );
